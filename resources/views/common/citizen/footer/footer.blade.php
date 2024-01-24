58f5a023db857aa77b8de0fdc12189e56ccc068f
govind66301~ <whitdevil06@gmail.com>
2024-01-24 15:48:32 +0530

    Version - 1.4

--- resources/views/common/citizen/footer/footer.blade.php
+++ resources/views/common/citizen/footer/footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="footer " style="color: white; background:#086070;">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-12 text-center">
                 <b>Content Owned by Ulhasnagar Municipal Corporation. © {{ date('Y') }}</b>
             </div>
-            <div class="col-sm-6">
+            {{-- <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
                     <b>Design And Develope by Core Ocean Solutions LLP</b>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </footer>
