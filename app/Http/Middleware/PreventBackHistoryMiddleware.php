5f073c946b6f85aada216f3399d2519fcbdc1170
govind66301~ <whitdevil06@gmail.com>
2024-03-04 12:37:51 +0530

    Invoice_Generated - 2.11

--- app/Http/Middleware/PreventBackHistoryMiddleware.php
+++ app/Http/Middleware/PreventBackHistoryMiddleware.php
@@ -19,14 +19,17 @@ class PreventBackHistoryMiddleware
     {
         $response = $next($request);
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token');
-        $response->headers->set('X-Frame-Options', 'DENY');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token');
+        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+        $response->headers->set('Access-Control-Allow-Origin', 'SAMEORIGIN');
+        $response->headers->set('Cross-Origin-Resource-Policy', 'SAMEORIGIN');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Permitted-Cross-Domain-Policies', 'master-only');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
-        $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
+        $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0');
+        $response->headers->set('Permissions-Policy', 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()');
         $response->headers->set('Pragma', 'no-cache');
         $response->headers->set('Expires', 'Sat, 26 Jul 1997 05:00:00 GMT');
         return $response;
